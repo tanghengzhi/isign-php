bc5a74466dedb9f747bace603966e97e8d246541
Victor Tang <tanghengzhi@live.com>
2020-04-24 11:15:38 +0800

    database

--- data/conf/database.php
+++ data/conf/database.php
@@ -9,7 +9,7 @@ return [
     // 服务器地址
     'hostname' => '127.0.0.1',
     // 数据库名
-    'database' => 'www_cs2019_com',
+    'database' => 'isign-php',
     // 用户名
     'username' => 'root',
     // 密码
